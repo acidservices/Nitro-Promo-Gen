0f2a2b4604a2fbaf38cd437f5901735173b6e949
poemslol <reapsedreapsed@gmail.com>
2026-02-08 10:29:35 +0000

    Add keyless.lua script with free key

---
+++ keyless.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY = "FREEKEYLESS!!!"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/385fd9e21f00b6f983db5ad9fa9f4392e85264e468cf4c648a3aaf9a6810cb98/download"))()
